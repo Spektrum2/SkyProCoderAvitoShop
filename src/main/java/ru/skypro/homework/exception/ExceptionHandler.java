1fe3397aa6d73c66e6dcbc8b8fa0c5cd1ac5e0c8
admin <promitay@gmail.com>
2023-03-14 11:38:38 +0300

    Настройка endpoints для заказов fix

--- src/main/java/ru/skypro/homework/exception/ExceptionHandler.java
+++ src/main/java/ru/skypro/homework/exception/ExceptionHandler.java
@@ -10,18 +10,24 @@ public class ExceptionHandler {
     @org.springframework.web.bind.annotation.ExceptionHandler(ImageNotFoundException.class)
     public ResponseEntity<String> handlesImageNotFoundException(ImageNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(String.format("Волантер с id = %d не найден", e.getId()));
+                .body(String.format("Image с id = %d не найден", e.getId()));
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(AdsNotFoundException.class)
     public ResponseEntity<String> handlesAdsNotFoundException(AdsNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(String.format("Волантер с id = %d не найден", e.getId()));
+                .body(String.format("Товар с id = %d не найден", e.getId()));
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(AvatarNotFoundException.class)
     public ResponseEntity<String> handlesAvatarNotFoundException(AvatarNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(String.format("Волантер с id = %d не найден", e.getId()));
+                .body(String.format("Avatar с id = %d не найден", e.getId()));
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handlesUserNotFoundException(UserNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(String.format("Пользователь с id = %d не найден", e.getId()));
     }
 }
